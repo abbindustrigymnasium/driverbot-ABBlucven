ead7cd6dfbfcf4d0dcacd35be190f2aea4b6ec11
ABBlucven <54663158+ABBlucven@users.noreply.github.com>
2020-03-31 10:21:32 +0200

    Python Projekt

---
+++ Valprojekt.py
@@ -0,0 +1,69 @@
+Vänster = bool
+ 
+grön = {
+    "namn": "Gröngölingarna",
+    Vänster: True,
+    "Block": "Småpartierna",
+    min: 3,
+    max: 12
+}
+partikel = {
+    "namn": "Partikelpartiet",
+    Vänster: True,
+    "Block": "Småpartierna",
+    min: 2,
+    max: 8
+}
+mälar = {
+    "namn": "Mälarpartiet",
+    Vänster: False,
+    "Block": "Småpartierna",
+    min: 8,
+    max: 18
+}
+sjö = {
+    "namn": "Sjörövarpartiet",
+    Vänster: False,
+    "Block": "Småpartierna",
+    min: 3,
+    max: 12
+}
+extrem = {
+    "namn": "Extremisterna",
+    Vänster: False,
+    "Block": "Oljeblocket",
+    min: 3,
+    max: 6
+}
+maskin = {
+    "namn": "Maskinpartiet",
+    Vänster: True,
+    "Block": "Oljeblocket",
+    min: 12,
+    max: 22
+}
+framtid = {
+    "namn": "Framtidspartiet",
+    Vänster: False,
+    "Block": "Oljeblocket",
+    min: 12,
+    max: 18
+}
+allpart = {
+    "namn": "Allpartiet",
+    Vänster: True,
+    "Block": "Inget",
+    min: 20,
+    max: 34
+}
+ 
+partier = [
+    grön,
+    partikel,
+    mälar,
+    sjö,
+    extrem,
+    maskin,
+    framtid,
+    allpartiet
+]
